7eec1e0e1f67279cced548db0a1f677d655003d0
oodoabel <get2abeloodo@gmail.com>
2025-11-28 22:36:49 +0100

    skip internal changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,12 @@ import { type NextRequest } from "next/server";
 import { updateSession } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  
+  if (pathname === "/_not-found") {
+    return;
+  }
+  
   return await updateSession(request);
 }
 
@@ -12,8 +18,9 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - api (API routes)
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|api|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
